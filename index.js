e07478e45c51b6a4ec3e7d135cd2f4749b186dad
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-30 18:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,20 +9,83 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ var itemCost = {}
+ var max = Math.floor(100)
+ itemCost[itemName] = parseInt(Math.random(1,101) * max + 1)
+
+ cart.push(itemCost)
+
+ console.log(`${itemName} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  var cartSpread = ``
+  var output = 'In your cart, you have'
+  if (cart.length > 0){
+    for (var i = 0; i < cart.length; i++){
+      if (cart.length === 1){
+        for (var key in cart[i]){
+          var val = cart[i][key]
+        }
+        cartSpread = `${key} at $${val}`
+        console.log(`${output} ${cartSpread}.`)
+      } else if (cart.length === 2) {
+          for (var key in cart[i]){
+            var val = cart[i][key]
+              if (i === 0) {
+                cartSpread = `${key} at $${val} and `
+              } else {
+                cartSpread =  cartSpread + `${key} at $${val}`
+              }
+            }
+            console.log(`${output} ${cartSpread}.`)
+          } else if (cart.length > 2) {
+              for (var key in cart[i]){
+                var val = cart[i][key]
+                if (i < cart.length -1){
+                  cartSpread = cartSpread + `${key} at $${val}, `
+                } else {
+                  cartSpread = cartSpread + `and ${key} at $${val}`
+                }
+              }
+              console.log(`${output} ${cartSpread}.`)
+            }
+          }
+        } else {
+          console.log("Your shopping cart is empty.")
+        }
+
+
+
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for (var i = 0; i < cart.length; i++){
+    for (var key in cart[i]){
+      total = total + cart[i][key]
+    }
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var items = 0
+  for (var i = 0; i < cart.length; i++){
+    for (var key in cart[i]) {
+      if (item === key) {
+        delete cart[i]
+        console.log(cart)
+        return cart
+      } else {
+        items++
+      }
+    } if (items === cart.length){
+      console.log('That item is not in your cart.')
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
