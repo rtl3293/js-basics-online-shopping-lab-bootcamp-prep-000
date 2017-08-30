c93d3fd419e22fc64f3d26ecab370fffdbbb22e8
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-30 19:04:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ function placeOrder(cardNumber) {
   // write your code here
 }
 
-/*addToCart("apples")
+addToCart("apples")
 addToCart("pears")
 viewCart()
 removeFromCart("a")
-viewCart()*/
+viewCart()
